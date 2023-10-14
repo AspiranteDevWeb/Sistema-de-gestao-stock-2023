588fad88bcccc1abda40955c0bd3338b4a300514
Damaiceno Castro <cenocastro01@gmail.com>
2023-10-14 23:40:06 +0200

    adding access controll to normal user

--- src/component/router/router.js
+++ src/component/router/router.js
@@ -7,12 +7,19 @@ import Signup from "../pages/auth/signup/signup"
 import Signin from '../pages/auth/signin/signin'
 import UseAuth from '../pages/auth/components/hooks/useAuth'
 
+import Signun_change from "../pages/auth/signin_change/signin_change"
 //import './navBar_Geral.css'
 function Rotas() {
 
   const Private = ({Item})=>{
     const {signed} = UseAuth()
     return signed > 0 ?<Item/>:<Signin/>
+  }
+
+  const Priva=({Iten})=>{
+    const {online_usuarios}= UseAuth()
+    console.log (online_usuarios, 'rota Priva')
+    return online_usuarios === "lamp@gmail.com"?<Iten/>:<Tabela/>
   }
  
   return (
@@ -21,9 +28,10 @@ function Rotas() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Private Item={Tabela}/>}/>
-          <Route exact path="/Home" element={<Home/>}/>
+          <Route exact path="/Home" element={<Priva Iten={Home}/>}/>
           <Route exact path="/Signin" element={<Signin/>}/>
           <Route exact path="/Signup" element={<Signup/>}/>
+          <Route exact path="/mudar" element={<Signun_change/>}/>
           <Route path ="*" element={<Signin/>}/>
         </Routes>
       </BrowserRouter>
